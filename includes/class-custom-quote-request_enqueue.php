901b45ccf8f39b4a7a2da3fcfc0d6682da1206d3
vikAyz9rY4Y7gT5 <KHALIDLOGI@GMAIL.COM>
2024-06-03 18:25:10 +0100

    adas_quote_selected_categories_callback

--- includes/class-custom-quote-request_enqueue.php
+++ includes/class-custom-quote-request_enqueue.php
@@ -2,6 +2,13 @@
 class Custom_Bootstrap_Enqueue {
 	public static function init() {
 		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ) );
+		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'adas_quote_admin_styles' ) );
+	}
+
+
+	public static function adas_quote_admin_styles() {
+		// Enqueue custom CSS for the admin settings page
+		wp_enqueue_style( 'adas-quote-admin-styles', plugin_dir_url( __FILE__ ) . '../css/admin-styles.css' );
 	}
 
 	public static function enqueue_scripts() {
@@ -11,12 +18,12 @@ class Custom_Bootstrap_Enqueue {
 		// Enqueue Bootstrap CSS
 		wp_enqueue_style( 'bootstrap-css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css' );
 
-        // Enqueue custom CSS
+		// Enqueue custom CSS
 		wp_enqueue_style(
 			'custom-quote-style',
 			plugin_dir_url( __FILE__ ) . '../css/custom-quote.css'
 		);
-        
+
 		// Enqueue Bootstrap JS
 		wp_enqueue_script( 'bootstrap-js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js', array( 'jquery' ), null, true );
 	}
--- includes/class-wc-settings-adas-quote.php
+++ includes/class-wc-settings-adas-quote.php
@@ -17,25 +17,25 @@ function adas_quote_settings_menu() {
 
 add_action( 'admin_init', 'adas_quote_settings_init' );
 
-add_action( 'admin_init', 'adas_quote_handle_clear_settings' );
+// add_action( 'admin_init', 'adas_quote_handle_clear_settings' );
 
-function adas_quote_handle_clear_settings() {
-	if ( isset( $_POST['adas_quote_clear_settings'] ) && $_POST['adas_quote_clear_settings'] == '1' ) {
-		delete_option( 'adas_quote_hide_add_to_cart' );
-		delete_option( 'adas_quote_hide_price' );
-		delete_option( 'adas_quote_custom_email_message' );
-		delete_option( 'adas_quote_admin_email' );
-		delete_option( 'adas_quote_selected_products' );
-		delete_option( 'adas_quote_selected_categories' );
-	}
-}
+// function adas_quote_handle_clear_settings() {
+// if ( isset( $_POST['adas_quote_clear_settings'] ) && $_POST['adas_quote_clear_settings'] == '1' ) {
+// delete_option( 'adas_quote_hide_add_to_cart' );
+// delete_option( 'adas_quote_hide_price' );
+// delete_option( 'adas_quote_custom_email_message' );
+// delete_option( 'adas_quote_admin_email' );
+// delete_option( 'adas_quote_selected_products' );
+// delete_option( 'adas_quote_selected_categories' );
+// }
+// }
 function adas_quote_settings_init() {
 	register_setting( 'adas_quote_settings_group', 'adas_quote_hide_add_to_cart' );
 	register_setting( 'adas_quote_settings_group', 'adas_quote_hide_price' );
 	register_setting( 'adas_quote_settings_group', 'adas_quote_custom_email_message' );
 	register_setting( 'adas_quote_settings_group', 'adas_quote_admin_email' );
 	register_setting( 'adas_quote_settings_group', 'adas_quote_selected_products' );
-	// register_setting( 'adas_quote_settings_group', 'adas_quote_selected_categories' );
+	register_setting( 'adas_quote_settings_group', 'adas_quote_selected_categories' );
 
 	add_settings_section(
 		'adas_quote_settings_section',
@@ -45,13 +45,13 @@ function adas_quote_settings_init() {
 	);
 
 	// Add settings field for selecting categories
-	// add_settings_field(
-	// 'adas_quote_selected_categories',
-	// 'Select Categories for Quote Button',
-	// 'adas_quote_selected_categories_callback',
-	// 'adas-quote-settings',
-	// 'adas_quote_settings_section'
-	// );
+	add_settings_field(
+		'adas_quote_selected_categories',
+		'Select Categories for Quote Button',
+		'adas_quote_selected_categories_callback',
+		'adas-quote-settings',
+		'adas_quote_settings_section'
+	);
 
 	add_settings_field(
 		'adas_quote_hide_add_to_cart',
@@ -93,25 +93,23 @@ function adas_quote_settings_init() {
 	);
 }
 
-// function adas_quote_selected_products_callback() {
-// $selected_products = get_option( 'adas_quote_selected_products', array() );
-// $products          = wc_get_products(
-// array(
-// 'limit'     => -1, // Get all products
-// 'status'    => 'publish', // Only published products
-// 'post_type' => 'product',
-// )
-// );
+function adas_quote_selected_categories_callback() {
+	$selected_categories = get_option( 'adas_quote_selected_categories', array() );
+	$categories          = get_terms(
+		array(
+			'taxonomy'   => 'product_cat',
+			'hide_empty' => false,
+		)
+	);
 
-// echo '<select multiple name="adas_quote_selected_products[]" style="width: 300px;">';
-// foreach ( $products as $product ) {
-// $selected = in_array( $product->get_id(), $selected_products ) ? 'selected' : '';
-// $selected = 'selected';
+	echo '<select multiple name="adas_quote_selected_categories[]" style="width: 300px;">';
+	foreach ( $categories as $category ) {
+		$selected = in_array( $category->term_id, $selected_categories ) ? 'selected' : '';
+		echo '<option value="' . esc_attr( $category->term_id ) . '" ' . $selected . '>' . esc_html( $category->name ) . '</option>';
+	}
+	echo '</select>';
+}
 
-// echo '<option value="' . esc_attr( $product->get_id() ) . '" ' . $selected . '>' . esc_html( $product->get_title() ) . '</option>';
-// }
-// echo '</select>';
-// }
 function adas_quote_selected_products_callback() {
 	$selected_products = get_option( 'adas_quote_selected_products', array() );
 	$paged             = isset( $_REQUEST['paged'] ) ? absint( $_REQUEST['paged'] ) : 1;
@@ -207,17 +205,20 @@ function adas_quote_hide_add_to_cart_callback() {
 
 function adas_quote_settings_page() {
 	?>
-<div class="wrap">
-	<h1>ADAS Quote Settings</h1>
-	<form method="post" action="options.php">
-		<?php
+
+<div class="wrap custom-quote-settings-page">
+
+	<div class="wrap">
+		<h1>ADAS Quote Settings</h1>
+		<form method="post" action="options.php">
+			<?php
 			settings_fields( 'adas_quote_settings_group' );
 			do_settings_sections( 'adas-quote-settings' );
 			submit_button();
-		?>
-		<input type="hidden" name="adas_quote_clear_settings" value="1">
-		<?php submit_button( 'Clear All Settings', 'delete' ); ?>
-	</form>
+			?>
+
+		</form>
+	</div>
 </div>
 	<?php
 }
